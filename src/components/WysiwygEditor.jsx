ba2c064fc447d54af7aafa820d1e720cac6800ec
tovehydmark <tovehydmark@gmail.com>
2022-06-07 13:29:57 +0200

    title and author added

--- src/components/WysiwygEditor.jsx
+++ src/components/WysiwygEditor.jsx
@@ -1,20 +1,39 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 export function WysiwygEditor() {
+const [documentTitle, setDocumentTitle] = useState("")
+const [author, setAuthor] = useState("")
+
   const editorRef = useRef(null);
 
   const log = () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
+
+      //Här gör vi en post till api:et
     }
   };
 
+  function handleTitleChange(e) {
+    setDocumentTitle(e.target.value);
+  }
+  function handleAuthorChange(e){
+    setAuthor(e.target.value)
+  }
+
   return (
     <>
+    <h1>Titel: {documentTitle}</h1>
+    <input type="text" value={documentTitle} onChange={handleTitleChange}/>   
+    
+    <p>Författare: {author}</p>
+    <input type="text" value={author} onChange={handleAuthorChange}/> 
+
       <Editor
         onInit={(evt, editor) => (editorRef.current = editor)}
-        initialValue="<p>This is the initial content of the editor.</p>"
+        initialValue="<p>Skriv här...</p>"
+        
         init={{
           height: 500,
           menubar: false,
@@ -32,7 +51,9 @@ export function WysiwygEditor() {
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
       />
-      <button onClick={log}>KLICKA HÄR</button>
+      <button onClick={log}>Spara</button>
+      <button>Visa färdigt dokument</button>
+      <button>Tillbaka till startsidan</button>
     </>
   );
 }
