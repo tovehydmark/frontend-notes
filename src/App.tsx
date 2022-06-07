92cc36853742ee000435acf362db3db4f0d396f0
tovehydmark <tovehydmark@gmail.com>
2022-06-07 20:21:54 +0200

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { Editor } from "@tinymce/tinymce-react";
 import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
 // import logo from './logo.svg';
 import "./App.css";
+import { Layout } from "./components/Layout";
+import { Login } from "./components/Login";
+import { NotFound } from "./components/NotFound";
 import { ShowDocuments } from "./components/ShowDocuments";
 import { WysiwygEditor } from "./components/WysiwygEditor";
 
 function App() {
   return (
     <>
-      <WysiwygEditor></WysiwygEditor>
-      <ShowDocuments></ShowDocuments>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Login />} />
+            <Route path="/showdocuments" element={<ShowDocuments />} />
+            <Route path="/editor" element={<WysiwygEditor />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </>
   );
 }
