e46c4e0110aa7a005f92b5be1f1748b1dd5ef39a
tovehydmark <tovehydmark@gmail.com>
2022-06-09 14:09:38 +0200

    removed unnecessary log

--- src/components/DisplayDocument.tsx
+++ src/components/DisplayDocument.tsx
@@ -1,48 +1,23 @@
-import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Documents } from "../models/Document";
 
 import "../styles/style.scss";
 
 interface IDisplayDocumentProps {
   documentInfo: Documents;
-  // reRenderParent(): void;
 }
 
 export function DisplayDocument(props: IDisplayDocumentProps) {
-  // const [documentToDisplay, setDocumentToDisplay] = useState(
-  //   new Documents("", "", "", "", 0)
-  // );
-
-  // function updateParent() {
-  //   props.reRenderParent();
-  // }
-
-  // function getDocumentData() {
-  //   // updateParent();
-
-  //   let document = new Documents(
-  //     props.documentInfo.documentTitle,
-  //     props.documentInfo.documentText,
-  //     props.documentInfo.author,
-  //     props.documentInfo.date,
-  //     props.documentInfo.documentId
-  //   );
-  // setDocumentToDisplay(document);
-  // }
-
-  // useEffect(() => {
-  //   getDocumentData();
-  // }, []);
-
   return (
     <>
-      <div className="readingview-div">
+      <div className="reading-view">
         <h1>{props.documentInfo.documentTitle}</h1>
         <p>{props.documentInfo.author}</p>
         <p>{props.documentInfo.date}</p>
         <p>{props.documentInfo.documentText}</p>
       </div>
-      {/* <p>{documentToDisplay.author}</p> */}
+
+      <Link to={`/showdocuments`}>Tillbaka till alla dokument</Link>
     </>
   );
 }
