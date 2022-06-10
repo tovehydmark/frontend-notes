8def0121ed51c1ae6a265ca6bc250371399467cd
tovehydmark <tovehydmark@gmail.com>
2022-06-10 12:38:24 +0200

    controls if user is in database at login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,54 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FetchData } from "./FetchData";
 
 export function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {}, []);
+
+  //Compares user input with database data
+  function loginUser() {
+    console.log("klick");
+
+    let userObject = { userName: username, password: password };
+
+    FetchData("http://localhost:3003/login", "POST", userObject).then(
+      (data) => {
+        console.log(data);
+
+        if (data.status == 200) {
+          // Saves userId to localstorage to keep user loggedin
+          localStorage.setItem("user", JSON.stringify(data.userId));
+
+          //Redirects user to loggedin page
+        }
+      }
+    );
+  }
+
   return (
     <>
-      <h1>Hej Logga in då</h1>
-      <Link to={`/showdocuments`}>Visa dokument</Link>
-      <Link to={`/editor`}>Skapa nytt dokument</Link>
+      <h1>Logga in i dokumenthanteraren</h1>
+
+      <label htmlFor="username">Användarnamn: </label>
+      <input
+        type="text"
+        name="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <label htmlFor="password">Lösenord: </label>
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={loginUser}>Logga in </button>
     </>
   );
 }
